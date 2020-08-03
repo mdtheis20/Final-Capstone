4a35be1e33d31849c9516cd574b67f45c702da2b
Charles Klein <kleincharles312@gmail.com>
2020-08-03 15:03:20 -0400

    changed filter component categories to checkbox

--- vue/src/components/FilterBar.vue
+++ vue/src/components/FilterBar.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="filter" class="menu-bar">
       <label for="filter">Category</label>
-      <select name="category">
-          <option disabled>--choose categories--</option>
-          <option v-for="category in categories" :key="category.categoryID" :value="category.name">{{category.name}}</option>
-      </select>
-      <label for="searchTitle">Title</label>
+      <ul>
+          <li v-for="category in categories" :key="category.categoryID" >
+              
+              <input type="checkbox" :name="category.name" :value="category.name" v-model="filterSelections">
+              <label :for="category.name"> {{category.name}}</label>
+          </li>
+      </ul>
+      <label for="searchTitle">Title: </label>
       <input type="text" name="searchTitle" v-model="searchTitle" />      
   </div>
 </template>
@@ -22,11 +25,15 @@ export default {
                 {categoryID: 4, name: 'Having to do with the Browns'}
             ],
             searchTitle: '',
+            filterSelections: [],
         }
     }
 }
 </script>
 
-<style>
-
+<style scoped>
+div#filter li {
+    list-style-type: none;
+   /*  list-style-position: outside; */
+}
 </style>
