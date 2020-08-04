23293362a49cc2b54cd973256eb9655ab6a3fe8d
Amy Cave <amypcave@gmail.com>
2020-08-04 10:02:22 -0400

    item list issues

---
+++ vue/src/components/ItemCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="item-card">
+      
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            item: {
+                id: '',
+                title: '',
+                subtitle: '',
+                pic: '',
+                startingBid: 0,
+                category: '',
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
