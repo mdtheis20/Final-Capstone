601bccd13feb30e8ca7043870fb185afb9d25445
Matt Theis <mdtheis@gmail.com>
2020-08-04 11:03:25 -0400

    Working on Bids DAO

---
+++ dotnet/Capstone/DAO/BidSQLDAO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.DAO
+{
+    public class BidSQLDAO : IBidDAO
+    {
+        private readonly string connectionString;
+
+        public BidSQLDAO(string dbConnectionString)
+        {
+            connectionString = dbConnectionString;
+        }
+
+
+    }
