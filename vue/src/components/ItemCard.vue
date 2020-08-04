9fbf080e9aca5a6bf19815738643c47b43a37b25
Charles Klein <kleincharles312@gmail.com>
2020-08-04 09:51:25 -0400

    Added ability to display items on page

---
+++ vue/src/components/ItemCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="item-card">
+      <h3>{{item.title}}</h3>
+      <h4>{{item.startingBid}}</h4>      
+      <img :src="item.pic" :alt="item.subtitle" />
+      <!-- <p>{{item.subtitle}}</p> -->
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            /* item: {
+                itemID: Number,
+                title: '',
+                subtitle: '',
+                pic: '',
+                startingBid: Number,
+                category: []
+            } */
+            // TODO: display top bid instead of starting bid if there are bids
+            // TODO: display how many bids have benn placed
+        }
+    },
+    props: {
+        item: {
+                itemID: Number,
+                title: '',
+                subtitle: '',
+                pic: '',
+                startingBid: Number,
+                category: []
+            }
+    }
+}
+</script>
+
+<style>
+
+</style>
