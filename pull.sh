878dab58b2168a634fc2f8a670738ec50d9fa837
Max Michael <maxdsmichael@gmail.com>
2020-08-06 12:05:27 -0400

    Changed pull shell script

--- pull.sh
+++ pull.sh
@@ -1,1 +1,16 @@
 git pull
+
+#!/bin/bash
+echo "Press 'q' to exit"
+count=0
+while : ; do
+read -n 1 k <&1
+if [[ $k = q ]] ; then
+printf "\nQuitting from the program\n"
+break
+else
+((count=$count+1))
+printf "\nIterate for $count times\n"
+echo "Press 'q' to exit"
+fi
+done
