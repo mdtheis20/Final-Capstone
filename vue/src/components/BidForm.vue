d21bc7f6eb1ed9b24d7f8dace9bfbaa95699114e
Charles Klein <kleincharles312@gmail.com>
2020-08-06 10:50:38 -0400

    added html for bid form

---
+++ vue/src/components/BidForm.vue
@@ -0,0 +1,47 @@
+<template>
+  <form id="bid-form">
+    <div>
+      <input type="radio" name="easyButtons" class="radio-selector" />
+      <button>$1</button>
+      <button>$5</button>
+      <button>$10</button>
+      <button>$20</button>
+    </div>
+
+    <div>
+      <input type="radio" name="custom" class="radio-selector" />
+      <label for="amount">Enter Amount:</label>
+      <input type="number" name="amount" id="custom-bid-amount" />
+    </div>
+  </form>
+</template>
+
+<script>
+// TODO: items do not load if you go straight to the url without going to home
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    itemID: Number,
+  },
+};
+</script>
+
+<style scoped>
+#bid-form {
+    display: flex;
+    flex-wrap: wrap;
+}
+#bid-form input.radio-selector {
+  display: inline-block;
+  margin: 15px;
+  
+}
+button {
+  margin: 15px;
+}
+#custom-bid-amount {
+  margin: 15px;
+}
+</style>
