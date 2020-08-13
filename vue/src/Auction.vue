183270c4e0bd7b9343775866795fcfa1829a38e6
Max Michael <maxdsmichael@gmail.com>
2020-08-13 13:53:54 -0400

    Changed image urls in database, fixed tiling background gradient

--- vue/src/Auction.vue
+++ vue/src/Auction.vue
@@ -86,6 +86,10 @@ export default {
 <style>
 body {
   /* background-color: #1b262c; */
+  height: 100%;
+  margin: 0;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
   text-align: center;
   padding: 10px;
   background-image: linear-gradient(#0f4c75, #1b262c);
